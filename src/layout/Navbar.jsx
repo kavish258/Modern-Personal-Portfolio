55ce3540c34d7ca4116a856deb620a80bbcab04a
kavish258 <Kavishonline0@gmail.com>
2026-02-28 20:13:01 +0530

    setup and update code

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+    return <header>
+        <nav>
+            <a>
+                PM<span>.</span>
+            </a>
+            {/*Desktop Nav*/}
+            <div>
+                <div>
+                    
+                </div>
+
+            </div>
+        </nav>
+    </header>;
+};
