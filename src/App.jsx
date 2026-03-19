378d89fe1c2690cf60d8025248eadd187cd57731
kavish258 <Kavishonline0@gmail.com>
2026-03-20 01:27:38 +0530

    set contact section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Experience } from "./sections/Experience";
 import { Testimonials } from "./sections/Testimonials";
 import { Contact } from "./sections/Contact";
 import { Navbar } from "./layout/Navbar";
+import { Footer } from "./layout/footer";
 
 function App() {
   return <div className="min-h-screen overflow-x-hidden">
@@ -18,6 +19,7 @@ function App() {
       <Contact />
 
     </main>
+    <Footer />
   </div>
 }
 
