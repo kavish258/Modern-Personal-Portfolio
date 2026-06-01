68eff56ea8d7da452952a129895315e4c20d6420
kavish258 <kavishonline0@gmail.com>
2026-06-02 01:02:43 +0530

    set header

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -37,7 +37,7 @@ export const Experience = () => {
 
       <div className="container mx-auto px-6 relative z-10">
         {/* Section Header */}
-        <div className="max-w-3xl mb-16">
+        <div className="max-w-3xl mb-16 text-center">
           <span
             className="text-secondary-foreground text-sm
            font-medium tracking-wider uppercase animate-fade-in"
